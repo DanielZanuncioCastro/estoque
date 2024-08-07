567323dc2582c2038e896bf2b3a486432e8620d7
DanielZanuncioCastro <danielzanunciodiascastro@hotmail.com>
2024-08-07 18:28:47 +0000

    update

--- Estoque.cpp
+++ Estoque.cpp
@@ -16,12 +16,13 @@ void Estoque::add_mercadoria(const std::string& mercadoria, unsigned int qtd) {
 void Estoque::sub_mercadoria(const std::string& mercadoria, unsigned int qtd) {
   auto it = produtoqtd.find(mercadoria);
   if (it != produtoqtd.end()) {
-    int qtdantigo = it->second;
-    if (qtdantigo < qtd) {
+    // int qtdantigo = it->second;
+    if (it->second < qtd) {
       std::cout << mercadoria << "insuficiente" << std::endl;      
     }
     else 
     {
+      // it->second -= qtd;
       produtoqtd[mercadoria] -= qtd;
     }
 }
@@ -30,6 +31,22 @@ void Estoque::sub_mercadoria(const std::string& mercadoria, unsigned int qtd) {
     std::cout << mercadoria << "inexistente" << std::endl;
   }
 }
+// void Estoque::sub_mercadoria(const std::string& mercadoria, unsigned int qtd) {
+  
+//   if (produtoqtd.find(mercadoria) == produtoqtd.end()) {
+//     std::cout << mercadoria << "inexistente" << std::endl;
+//   }
+//   else{
+//     auto it = produtoqtd.find(mercadoria);
+    
+//     if (it->second > qtd) {
+//       produtoqtd[mercadoria] -= qtd;
+//     }
+//     else{
+//       std::cout << mercadoria << "insuficiente" << std::endl;      
+//     }    
+// }
+// }
 
 unsigned int Estoque::get_qtd(const std::string& mercadoria) const {
   auto it = produtoqtd.find(mercadoria);
@@ -41,17 +58,18 @@ unsigned int Estoque::get_qtd(const std::string& mercadoria) const {
 }
 
 void Estoque::imprime_estoque() const {
-  for (auto it = produtoqtd.begin(); it != produtoqtd.end(); it++) {
-    const std::string& mercadoria = it->first;
-    unsigned int qtd = it->second; 
+  for(auto it : produtoqtd){
+  // for (auto it = produtoqtd.begin(); it != produtoqtd.end(); it++) {
+    const std::string& mercadoria = it.first;
+    unsigned int qtd = it.second; 
     std::cout << mercadoria << ", " << qtd << std::endl;
   }
 }
 
 Estoque& Estoque::operator += (const Estoque& rhs) {
-  for(auto it : rhs.produtoqtd)
-    {
-      add_mercadoria(it.first, it.second);
+  for (auto it = rhs.produtoqtd.begin(); it != rhs.produtoqtd.end(); it++) {
+  // for(auto it : rhs.produtoqtd) {
+      add_mercadoria(it->first, it->second);
     }
   return *this;
 }
@@ -64,20 +82,38 @@ Estoque& Estoque::operator -= (const Estoque& rhs) {
   return *this;
 }
 
-bool operator<(Estoque& lhs, Estoque& rhs) {
-    // Itera sobre todos os produtos em lhs
-    for (const auto& item : lhs.produtoqtd) {
-        const std::string& mercadoria = item.first;
-        int qtd1 = item.second;
+// bool operator<(Estoque& lhs, Estoque& rhs) {
+//     // Itera sobre todos os produtos em lhs
+//     for (const auto& item : lhs.produtoqtd) {
+//         const std::string& mercadoria = item.first;
+//         int qtd1 = item.second;
 
+//         // Verifica se o produto está presente em rhs
+//         auto it = rhs.produtoqtd.find(mercadoria);
+//         if (it == rhs.produtoqtd.end()) {
+//             // Produto de lhs não está presente em rhs
+//             return false;
+//         } else {
+//             int qtd2 = it->second;
+//             if (qtd1 >= qtd2) {
+//                 // Quantidade em lhs não é menor do que em rhs
+//                 return false;
+//             }
+//         }
+//     }
+//     // Se todos os itens em lhs forem encontrados em rhs com quantidade menor, então lhs < rhs
+//     return true;
+// }
+
+bool operator<(Estoque& e0, Estoque& e1) {
+    for (auto it : e0.produtoqtd) {        
         // Verifica se o produto está presente em rhs
-        auto it = rhs.produtoqtd.find(mercadoria);
-        if (it == rhs.produtoqtd.end()) {
+        auto item = e1.produtoqtd.find(it.first);
+        if (item == e1.produtoqtd.end()) {
             // Produto de lhs não está presente em rhs
             return false;
-        } else {
-            int qtd2 = it->second;
-            if (qtd1 >= qtd2) {
+        } else {            
+            if (item->second <= it.second) {
                 // Quantidade em lhs não é menor do que em rhs
                 return false;
             }
@@ -87,25 +123,43 @@ bool operator<(Estoque& lhs, Estoque& rhs) {
     return true;
 }
 
-bool operator>(Estoque& lhs, Estoque& rhs) {
-    // Itera sobre todos os produtos em rhs
-    for (const auto& item : rhs.produtoqtd) {
-        const std::string& mercadoria = item.first;
-        int qtd2 = item.second;
+// bool operator>(Estoque& lhs, Estoque& rhs) {
+//     // Itera sobre todos os produtos em rhs
+//     for (const auto& item : rhs.produtoqtd) {
+//         const std::string& mercadoria = item.first;
+//         int qtd2 = item.second;
 
-        // Verifica se o produto está presente em lhs
-        auto it = lhs.produtoqtd.find(mercadoria);
-        if (it == lhs.produtoqtd.end()) {
-            // Produto de rhs não está presente em lhs
+//         // Verifica se o produto está presente em lhs
+//         auto it = lhs.produtoqtd.find(mercadoria);
+//         if (it == lhs.produtoqtd.end()) {
+//             // Produto de rhs não está presente em lhs
+//             return false;
+//         } else {
+//             int qtd1 = it->second;
+//             if (qtd1 <= qtd2) {
+//                 // Quantidade em lhs não é maior do que em rhs
+//                 return false;
+//             }
+//         }
+//     }
+//     // Se todos os itens em rhs forem encontrados em lhs com quantidade maior, então lhs > rhs
+//     return true;
+// }
+
+bool operator>(Estoque& e0, Estoque& e1) {
+    for (auto it : e1.produtoqtd) {        
+        // Verifica se o produto está presente em rhs
+        auto item = e0.produtoqtd.find(it.first);
+        if (item == e0.produtoqtd.end()) {
+            // Produto de lhs não está presente em rhs
             return false;
-        } else {
-            int qtd1 = it->second;
-            if (qtd1 <= qtd2) {
-                // Quantidade em lhs não é maior do que em rhs
+        } else {            
+            if (item->second <= it.second) {
+                // Quantidade em lhs não é menor do que em rhs
                 return false;
             }
         }
     }
-    // Se todos os itens em rhs forem encontrados em lhs com quantidade maior, então lhs > rhs
+    // Se todos os itens em lhs forem encontrados em rhs com quantidade menor, então lhs < rhs
     return true;
 }
